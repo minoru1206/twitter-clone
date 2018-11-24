f07f81f096a653404b7f69df1a5be124897b06d1
EC2 Default User <ec2-user@ip-172-31-56-21.ec2.internal>
2018-11-24 11:59:30 +0000

    init Laravel Project

--- database/migrations/2018_11_24_102639_add_status_to_tasks_table.php
+++ database/migrations/2018_11_24_102639_add_status_to_tasks_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateTasklistsTable extends Migration
+class AddStatusToTasksTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,8 @@ class CreateTasklistsTable extends Migration
      */
     public function up()
     {
-        Schema::create('tasklisrts', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('content');
-            $table->timestamps();
+        Schema::table('tasks', function (Blueprint $table) {
+            $table->string('status',10);
         });
     }
 
@@ -27,6 +25,8 @@ class CreateTasklistsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('tasklisrts');
+        Schema::table('tasks', function (Blueprint $table) {
+            $table->dropColumn('status',10);
+        });
     }
 }
