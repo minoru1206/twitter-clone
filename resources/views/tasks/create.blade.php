00a25b7049d34db746b31b980a1a28da2eda501d
EC2 Default User <ec2-user@ip-172-31-56-21.ec2.internal>
2018-11-25 09:41:53 +0000

    bootstrap design

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -10,17 +10,25 @@
     @endforeach
 </ul>
 @endif
-
+<div class='row'>
+    <div class='col-xs-12 col-sm-8 col-md-8 col-lg-6'>
+    <div class='col-sm-offset-2 col-md-offset-2 col-lg-offset-3'></div>
 {!! Form::model($task, ['route'=> 'tasks.store']) !!}
 
+<div class='form-group'>
 {!!Form::label('status','ステータス:')!!}
-{!!Form::text('status')!!}
+{!!Form::text('status',null,['class'=>'form-control'])!!}
+</div>
 
+<div class='form-group'>
 {!! Form::label('content','タスク:') !!}
-{!! Form::text('content') !!}
+{!! Form::text('content',null,['class'=>'form-control']) !!}
+</div>
 
-{!! Form::submit('投稿') !!}
+{!! Form::submit('投稿',['class'=>'btn btn-primary']) !!}
 
 {!! Form::close() !!}
+</div>
+</div>
 
 @endsection
